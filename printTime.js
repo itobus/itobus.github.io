f60016cfdc7d7b4cc2e9ad0b26c2555972e97575
Haichen Wen <haichenwen@HaichendeMacBook-Pro.local>
2018-09-01 17:31:51 +0900

    Initial commit

---
+++ printTime.js
@@ -0,0 +1,13 @@
+function printTime() {
+    let date = new Date()
+    let weekday = new Array(7)
+    weekday[0] = "Sunday"
+    weekday[1] = "Monday"
+    weekday[2] = "Tuesday"
+    weekday[3] = "Wednesday"
+    weekday[4] = "Thursday"
+    weekday[5] = "Friday"
+    weekday[6] = "Saturday"
+    document.getElementById("time").innerHTML=date.getMonth() + "月" + date.getDay() + "日  " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "  " + weekday[date.getDay()]
+    setTimeout(printTime, 1000)
+}
